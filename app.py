da822ee34187cb96b35b920cf66391da1660ee2d
Yash Shah <55008416+yash3108@users.noreply.github.com>
2021-04-10 14:10:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ import cv2
 from keras import models
 import numpy as np
 
-def process(str):
-    str = str.replace('[', '')
-    str = str.replace(']', '')
-    str = str.split(', ')
-    lst = list(map(int, str))
-    #print(len(lst))
-    arr = np.array(lst)
-    arr = arr.reshape(200, 200, 3)
-    #print(type(arr[0,0][0]), type(img1[0,0][0]))
-    #print((img1 == arr).all())
-    arr = arr.astype('uint8')
-    image = cv2.resize(arr, (64, 64))
-    image = np.array(image)
-    image = image.astype('float32')/255.0
-    image = image.reshape(-1, 64, 64, 3)
-    return image
+# def process(str):
+#     str = str.replace('[', '')
+#     str = str.replace(']', '')
+#     str = str.split(', ')
+#     lst = list(map(int, str))
+#     #print(len(lst))
+#     arr = np.array(lst)
+#     arr = arr.reshape(200, 200, 3)
+#     #print(type(arr[0,0][0]), type(img1[0,0][0]))
+#     #print((img1 == arr).all())
+#     arr = arr.astype('uint8')
+#     image = cv2.resize(arr, (64, 64))
+#     image = np.array(image)
+#     image = image.astype('float32')/255.0
+#     image = image.reshape(-1, 64, 64, 3)
+#     return image
 
 # load model
 model = pickle.load(open('model.pkl','rb'))
@@ -36,9 +36,9 @@ def predict():
     # get data
     data = request.get_json(force=True)
     
-    image = process(data)
+#     image = process(data)
 #     my_model.predict(image)
-    x = np.argmax(my_model.predict(image), axis=1)
+#     x = np.argmax(my_model.predict(image), axis=1)
 #     # convert data into dataframe
 #     data.update((x, [y]) for x, y in data.items())
 #     data_df = pd.DataFrame.from_dict(data)
