1eb729d862bfadf588050a54bc271ef140bc22b9
Yash Shah <55008416+yash3108@users.noreply.github.com>
2021-05-23 20:29:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
+import pickle5 as pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
